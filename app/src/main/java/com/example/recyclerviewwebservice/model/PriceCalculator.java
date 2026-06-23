0b5e29171dd5bc9307e370e72dc8e19d6c5e31a4
Younis Al-Sayed <yuns20055@gmail.com>
2026-06-23 17:53:43 +0300

    update to the logic of pricing

--- app/src/main/java/com/example/recyclerviewwebservice/model/PriceCalculator.java
+++ app/src/main/java/com/example/recyclerviewwebservice/model/PriceCalculator.java
@@ -2,7 +2,7 @@ package com.example.recyclerviewwebservice.model;
 
 public final class PriceCalculator {
     private static final int MIN_PRICE_CENTS = 799;
-    private static final int PRICE_RANGE_CENTS = 9_201;
+    private static final int PRICE_RANGE_CENTS = 9201;
     private static final int POSITION_BASE = 26;
     private static final int ROLLING_MULTIPLIER = 31;
 
@@ -10,14 +10,8 @@ public final class PriceCalculator {
     }
 
     public static int forProductId(String productId) {
-        long dynamicValue = 0L;
-        for (int index = 0; index < productId.length(); index++) {
-            int position = index + 1;
-            int characterValue = productId.charAt(index) % (POSITION_BASE * position);
-            dynamicValue = (
-                    dynamicValue * ROLLING_MULTIPLIER + characterValue
-            ) % PRICE_RANGE_CENTS;
-        }
-        return MIN_PRICE_CENTS + (int) dynamicValue;
+        long value = 0L;
+        value = (productId.hashCode() < 0 ? productId.hashCode() * -1 : productId.hashCode()) % PRICE_RANGE_CENTS;
+        return MIN_PRICE_CENTS + (int) value;
     }
 }
